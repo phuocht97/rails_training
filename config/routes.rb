422bb42e43ef291ac747bbdc172680938f980900
Ngo Trung Hung <trunghung5055@gmail.com>
2020-06-28 09:23:34 +0700

    Add RSpec into Gemfile & do exercise
    chapter3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
   resources :microposts
-  resources :m_icroposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
